7fa7c353f84f3c3f67f8830c9e12ca882d921e10
Kainar <kainar.ilyasov@gmail.com>
2019-05-26 23:49:59 +0600

    refactored code

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.searchInput = React.createRef();
+  }
+
+  componentDidMount() {
+    this.searchInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div className="pa2">
+        <input
+          className="pa3 ba b--green bg-lightest-blue"
+          type="search"
+          placeholder="Search Robots"
+          onChange={this.props.searchChange}
+          ref={this.searchInput}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
