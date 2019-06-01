d591eb7ebc30243a8b56a6b431ec19bb1b42d7a2
Kainar92 <kainar.ilyasov@gmail.com>
2019-06-01 18:12:42 +0600

    Updates

---
+++ precache-manifest.ab30201630bd693daee36d420266e989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5809c602a30ac9880e8ae640c8c93eba",
+    "url": "/robofriendsapp/index.html"
+  },
+  {
+    "revision": "bb6b5ced5edd474d46b2",
+    "url": "/robofriendsapp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0a6be599c432d6fc4481",
+    "url": "/robofriendsapp/static/css/main.7df764c9.chunk.css"
+  },
+  {
+    "revision": "bb6b5ced5edd474d46b2",
+    "url": "/robofriendsapp/static/js/2.80bfaca1.chunk.js"
+  },
+  {
+    "revision": "0a6be599c432d6fc4481",
+    "url": "/robofriendsapp/static/js/main.03ca68d3.chunk.js"
+  },
+  {
+    "revision": "8f2c2ea2dbc031d097a7",
+    "url": "/robofriendsapp/static/js/runtime~main.d4dbc020.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
